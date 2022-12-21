ce4560fa2b0f81f9246db8b81f722f98f44646f5
Testdk2 <120616619+Testdk2@users.noreply.github.com>
2022-12-21 09:50:14 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ Creator : <a href='https://telegram.dog/R_KOHLI'>𝚁𝙰𝚅𝙸 𝙺𝙾𝙷𝙻𝙸</a>\n┣⪼ Language : Python3\n┣⪼ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┣⪼ Source Code : <a href='https://github.com/Dkmovie/DK-Files-Store-Bot'>DK-File Store Bot</a>\n┣⪼ Channel : <a href='https://t.me/DK_Gru'>𝘿𝙆_𝙈𝙊𝙑𝙄𝙀</a>\n┣⪼ how to mack bot : <a href='https://youtu.be/lIweNNObiy0'>Opus Techz</a>\n╰━━━━━━━━━━━━━━━➣</b>",
+            text = f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ Creator : <a href='https://telegram.dog/R_KOHLI'>𝚁𝙰𝚅𝙸 𝙺𝙾𝙷𝙻𝙸</a>\n┣⪼ Language : Python3\n┣⪼ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┣⪼ Source Code : <a href='https://github.com/Dkmovie/DK-Files-Store-Bot'>DK-File Store Bot</a>\n┣⪼ CHANNL : <a href='https://t.me/movie_a1'>𝙳𝙺_𝙼𝙾𝚅𝙸𝙴</a>\n┣⪼ GROUP : <a href='https://t.me/movie_on1'>𝙼𝙾𝚅𝙸𝙴 𝙶𝚁𝙾𝚄𝙿</a>\n╰━━━━━━━━━━━━━━━➣</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
